462763b734a17833e08576605c5735ab8acbcfdd
test <test@example.com>
2022-12-14 21:29:50 +0530

    1.6

--- main.tf
+++ main.tf
@@ -2,56 +2,56 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "172.16.0.0/16"
-  instance_tenancy = "default"
-  tags = {
-    Name = "main"
-  }
-}
+# resource "aws_vpc" "main" {
+#   cidr_block = "172.16.0.0/16"
+#   instance_tenancy = "default"
+#   tags = {
+#     Name = "main"
+#   }
+# }
 
-#Create security group with firewall rules
-resource "aws_security_group" "jenkins-sg-2022" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
+# #Create security group with firewall rules
+# resource "aws_security_group" "jenkins-sg-2022" {
+#   name        = var.security_group
+#   description = "security group for Ec2 instance"
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  # outbound from jenkis server
+#   egress {
+#     from_port   = 0
+#     to_port     = 65535
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags= {
-    Name = var.security_group
-  }
-}
+#   tags= {
+#     Name = var.security_group
+#   }
+# }
 
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  associate_public_ip_address=true
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
-  tags= {
-    Name = var.tag_name
-  }
-}
+# resource "aws_instance" "myFirstInstance" {
+#   ami           = var.ami_id
+#   key_name = var.key_name
+#   instance_type = var.instance_type
+#   associate_public_ip_address=true
+#   vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+#   tags= {
+#     Name = var.tag_name
+#   }
+# }
 
 # # Create Elastic IP address
 # resource "aws_eip" "myFirstInstance" {
@@ -63,7 +63,7 @@ resource "aws_instance" "myFirstInstance" {
 # }
 
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.myFirstInstance.public_ip
-}
+# output "instance_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.myFirstInstance.public_ip
+# }
