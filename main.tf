05af5960a2a975e2364f34d25b40413138e96035
test <test@example.com>
2022-12-14 20:56:57 +0530

    1.4

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_instance" "myFirstInstance" {
 # }
 
 
-# output "instance_public_ip" {
-#   description = "Public IP address of the EC2 instance"
-#   value       = aws_instance.myFirstInstance.public_ip
-# }
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.myFirstInstance.public_ip
+}
